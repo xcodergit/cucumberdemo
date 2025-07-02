9fe7c1ace38000355c81bd2a2ba92c08670ef1d8
KK LABS <kklabsinfo@gmail.com>
2025-07-02 19:43:25 +0530

    dfg

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,6 +1,5 @@
 package hooks;
 
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -11,25 +10,25 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class Hooks {
-	
-	public static WebDriver driver;
+
+    public static WebDriver driver;
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-    
-        
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("window-size=1920,1080"); // for headless
+
         driver = new ChromeDriver(options);
-        
-        
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-      
     }
 
     @After
@@ -38,5 +37,4 @@ public class Hooks {
             driver.quit();
         }
     }
-
 }
